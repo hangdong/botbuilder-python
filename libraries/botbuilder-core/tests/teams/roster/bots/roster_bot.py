d55508e002bb0ffbe334952c1cf4ba611162c52f
virtual-josh <ratliff.joshua@gmail.com>
2019-11-19 15:21:20 -0800

    saving work 3:21

--- libraries/botbuilder-core/tests/teams/roster/bots/roster_bot.py
+++ libraries/botbuilder-core/tests/teams/roster/bots/roster_bot.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 from botbuilder.core import MessageFactory, TurnContext
-from botbuilder.core.teams import TeamsActivityHandler
+from botbuilder.core.teams import TeamsActivityHandler, TeamsInfo
 from botbuilder.schema.teams import ChannelInfo, TeamInfo, TeamsChannelAccount
 
 
@@ -34,6 +34,29 @@ class RosterBot(TeamsActivityHandler):
             return
 
     async def _show_members_activity(turn_context: TurnContext):
-        team_id = TurnContext.teams_get_team_info().id
+        teams_channel_account = await TeamsInfo.get_members(turn_context)
+        await turn_context.send_activity(MessageFactory.text(f"There are {len(teams_channel_account)} members are currently in the team"))
 
-        
+        messages = [f"{member.aad_group_id} --> {member.name} --> {member.user_principal_name}" for member in teams_channel_account]
+        a
+    
+    async def _show_channels_activity(turn_context: TurnContext):
+        pass
+    
+    async def _show_details_activity(turn_context: TurnContext):
+        team_id = TurnContext.teams_get_team_info().id
+        team_details = await TeamsInfo.get_team_details(turn_context, team_id)
+        reply_activity = MessageFactory.text(f"""The team name is {team_details.name}. 
+                                                The team ID is {team_details.id}. 
+                                                The AADGroupID is {team_details.aad_group_id}""")
+        await turn_context.send_activity(reply_activity)
+
+    async def _send_in_batches(turn_context: TurnContext, messages: []):
+        batch = []
+        for message in messages:
+            batch.append(message)
+            if len(batch) == 10:
+                await turn_context.send_activity(MessageFactory.text(batch.join("<br>")))
+                batch = []
+        if len(batch) > 0:
+            await turn_context.send_activity(MessageFactory.text(batch.join("<br>")))
